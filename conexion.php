beaa830d7766d4ba3f15917a5ec2d8b55bf525bf
jony-0502 <ezequiel.bascouer99@gmail.com>
2026-03-17 12:59:37 -0300

    Remove database connection code
    Removed database connection code from conexion.php.

--- conexion.php
+++ conexion.php
@@ -1,14 +1,1 @@
-<?php
 
-$servername = "localhost";
-$username = "mundocancan";
-$password = "ZY5+l]MLFn,[";
-$database = "mundocan_pel_can";
-
-$conectar = mysqli_connect($servername, $username, $password, $database);
-
-if (!$conectar) {
-    die("Error de conexión: " . mysqli_connect_error());
-}
-
-mysqli_set_charset($conectar, "utf8");
